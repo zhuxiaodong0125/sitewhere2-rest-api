09e4d86feac594b61b874bfbd59d36e1c2a36c35
Derek Adams <derek.adams@sitewhere.com>
2018-07-05 17:39:24 -0400

    Updated device state search parameters.

--- src/sitewhere-device-state-api.js
+++ src/sitewhere-device-state-api.js
@@ -8,9 +8,11 @@ import {
  */
 export function searchDeviceStates (axios, criteria, options) {
   let query = randomSeedQuery()
+  query += (options.includeDevice) ? '&includeDevice=true' : ''
   query += (options.includeDeviceType) ? '&includeDeviceType=true' : ''
   query += (options.includeCustomer) ? '&includeCustomer=true' : ''
   query += (options.includeArea) ? '&includeArea=true' : ''
   query += (options.includeAsset) ? '&includeAsset=true' : ''
+  query += (options.includeEventDetails) ? '&includeEventDetails=true' : ''
   return restAuthPost(axios, 'devicestates/search' + query, criteria)
 }
