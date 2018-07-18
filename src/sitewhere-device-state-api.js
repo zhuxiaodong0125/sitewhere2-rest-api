1780ae7e18b3e074465b49e9bd8c73ccf883f582
Derek Adams <derek.adams@sitewhere.com>
2018-07-18 16:15:18 -0400

    Added option for returning assignment info on device state query.

--- src/sitewhere-device-state-api.js
+++ src/sitewhere-device-state-api.js
@@ -10,6 +10,7 @@ export function searchDeviceStates (axios, criteria, options) {
   let query = randomSeedQuery()
   query += (options.includeDevice) ? '&includeDevice=true' : ''
   query += (options.includeDeviceType) ? '&includeDeviceType=true' : ''
+  query += (options.includeDeviceAssignment) ? '&includeDeviceAssignment=true' : ''
   query += (options.includeCustomer) ? '&includeCustomer=true' : ''
   query += (options.includeArea) ? '&includeArea=true' : ''
   query += (options.includeAsset) ? '&includeAsset=true' : ''
